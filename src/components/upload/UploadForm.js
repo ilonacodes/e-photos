67ee00cd7ef3ded98483f46a0ba20b08691cd78a
Ilona Demidenko <demiluri@gmail.com>
2017-07-22 23:36:02 +0200

    Markup upload form

--- src/components/upload/UploadForm.js
+++ src/components/upload/UploadForm.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export const UploadForm = () => {
-    return <div>
-        Uploading photos...
-        <input type="file" />
+    return <div className="upload-container">
+        <div className="title">
+            <h2>Upload Photo</h2>
+        </div>
+        <div className="input-form">
+            <label>Photo name</label>
+            <input type="text"/>
+        </div>
+        <div className="input-form">
+            <label>Tags (separated by comma)</label>
+            <input type="text"/>
+        </div>
+        <div className="upload-file">
+            <input type="file"/>
+        </div>
+        <div className="upload-button">
+            <button>Upload</button>
+        </div>
+
     </div>
 }
