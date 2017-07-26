154066c41437761aec8c980661308ce0ead9ae61
Ilona Demidenko <demiluri@gmail.com>
2017-07-26 21:44:04 +0200

    Configure activerecord so that it picks up on DATABASE_URL

--- backend/lib/db/init_active_record.rb
+++ backend/lib/db/init_active_record.rb
@@ -6,11 +6,14 @@ ENV['ENV'] ||= 'development'
 root = File.expand_path '../../..', __FILE__
 config = YAML.load(File.read(File.join(root, 'config/database.yml')))[ENV['ENV']]
 
-# Change the following to reflect your database settings
-ActiveRecord::Base.establish_connection(
-    adapter:  config['adapter'], # or 'postgresql' or 'sqlite3' or 'oracle_enhanced'
-    host:     config['host'],
-    database: config['database'],
-    username: config['username'],
-    password: config['password']
-)
+if ENV['DATABASE_URL']
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+else
+  ActiveRecord::Base.establish_connection(
+      adapter:  config['adapter'], # or 'postgresql' or 'sqlite3' or 'oracle_enhanced'
+      host:     config['host'],
+      database: config['database'],
+      username: config['username'],
+      password: config['password']
+  )
+end
