6ca429abc66fad05dd0672ea6494051e73021a94
Ilona Demidenko <demiluri@gmail.com>
2017-07-26 23:37:28 +0200

    set -x on run feature tests script

--- scripts/run_feature_tests.sh
+++ scripts/run_feature_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e +x
+set -e -x
 
 # kill all sub-processes automatically when this script exits for any reason
 trap "trap - SIGTERM && kill -- -$$ || true" SIGINT SIGTERM EXIT
