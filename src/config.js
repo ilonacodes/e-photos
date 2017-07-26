e71f5085162c1babd4e7546ba80c6064064b8e19
Ilona Demidenko <demiluri@gmail.com>
2017-07-26 22:18:04 +0200

    Configure production build for frontend

--- src/config.js
+++ src/config.js
@@ -1,3 +1,13 @@
-export const config = {
-    apiUrl: "http://localhost:4567"
+const environment = process.env.NODE_ENV
+
+const all_configs = {
+    development: {
+        apiUrl: "http://localhost:4567",
+    },
+
+    production: {
+        apiUrl: "https://ephotos.herokuapp.com",
+    }
 }
+
+export const config = all_configs[environment] || all_configs.development
