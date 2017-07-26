1b898069e1a8ca24a357d2d65eb3db63eedc50a1
Ilona Demidenko <demiluri@gmail.com>
2017-07-26 19:42:13 +0200

    Add specific language to build matrix and add xvfb to node tests

--- scripts/run_node_tests.sh
+++ scripts/run_node_tests.sh
@@ -2,5 +2,15 @@
 
 set -e
 
+node -v
+npm -v
+
 npm install
+
+export DISPLAY=:99.0
+
+sh -e /etc/init.d/xvfb start
+
+sleep 3                             # give xvfb some time to start
+
 npm run test-ci
