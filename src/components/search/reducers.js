2bb580ae3ee387b937f3a941145a1fd8dd248a03
Ilona Demidenko <demiluri@gmail.com>
2017-07-27 10:47:37 +0200

    Reset search results when route changes

--- src/components/search/reducers.js
+++ src/components/search/reducers.js
@@ -1,9 +1,13 @@
 import {t} from "./actions"
+import {LOCATION_CHANGE} from 'react-router-redux'
 
 export const searchReducer = (state = [], action = {}) => {
     switch (action.type) {
         case t.SEARCH_RESULTS:
             return action.payload
+
+        case LOCATION_CHANGE:
+            return []
 
         default:
             return state
